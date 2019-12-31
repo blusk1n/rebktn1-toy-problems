d66058e34d256920c3f6b6a399dae55138890d3d
Hamam-Elmuratdh <hamam.alsaiity@gmail.com>
2019-12-31 08:56:58 +0100

    solved

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,12 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+
+function zeroSum(arr){
+  for(var i = 0 ; i<arr.length;i++){
+      if(arr.indexOf(arr[i]*-1)!==-1){
+          return true
+      }
+  }
+  return false
+}
