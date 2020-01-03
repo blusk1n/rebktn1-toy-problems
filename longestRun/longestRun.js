d77f907889500e74a2c54642bdb9c3109980d11d
blusk1n <tahaznazen@gmail.com>
2020-01-03 09:08:18 +0100

    solved

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -12,4 +12,30 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+function longestRun(string) {
+    var arr = string.split("")
+    var obj = {};
+    if(string === ""){
+        return [0,0]
+    } 
+    for(var i =0 ; i<arr.length;i++){
+        let counter = arr[i-1];
+        if(obj[arr[i]] && arr[i]===counter){
+            obj[arr[i]]++
+        }
+        else{
+            obj[arr[i]]=1
+        }
+    }
+    var result = [];
+    for (var key in obj){ 
+        if(obj[key]>1){
+            result.push(key)
+        }
+        result.sort((a,b)=>{return obj[b]-obj[a]})    
+    }
+    if(result.length === 0){
+        return [0,0]
+    }
+    return [string.indexOf(result[0]),string.indexOf(result[0])-1+obj[result[0]]]
+}
